9f12130d021339ec1833661eec08ce3116beb19e
mohammed <mohammed@domain.com>
2025-07-23 08:34:55 +0400

    testing by creating account object and calling show method, an added space in diplay method for clarity

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Choose the operation: \n 1. Create a new account \n 2. Deposit money \n 3. Withdraw money\n 4. View account details \n 5. Exit");
-        Scanner sc = new Scanner(System.in);
-        int userChoice = sc.nextInt();
-        switch (userChoice){
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            default:
-                System.out.println("Invalid choice.");
-        }
+//        System.out.println("Choose the operation: \n 1. Create a new account \n 2. Deposit money \n 3. Withdraw money\n 4. View account details \n 5. Exit");
+//        Scanner sc = new Scanner(System.in);
+//        int userChoice = sc.nextInt();
+//        switch (userChoice){
+//            case 1:
+//                break;
+//            case 2:
+//                break;
+//            case 3:
+//                break;
+//            case 4:
+//                break;
+//            case 5:
+//                break;
+//            default:
+//                System.out.println("Invalid choice.");
+//        }
+
+        bankAccount user1= new bankAccount("user1", "112", 100);
+        bankAccount user2= new bankAccount("user2", "112", 100);
+
+
+        user1.showAccountInfo();
+        user2.showAccountInfo();
 
     }
 }
