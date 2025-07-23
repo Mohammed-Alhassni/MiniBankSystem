da910274d414dca6a13aad083f505df6bb078352
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2025-07-23 08:31:55 +0400

    created bankAccount with varibles, constructor and display method

--- src/bankAccount.java
+++ src/bankAccount.java
@@ -1,2 +1,18 @@
 public class bankAccount {
+    String accountName;
+    String accountNumber;
+    double balance;
+
+    public bankAccount(String accountName, String accountNumber, double balance) {
+        this.accountName=accountName;
+        this.accountNumber=accountNumber;
+        this.balance=balance;
+    }
+
+
+    public void showAccountInfo(){
+        System.out.println("Account Name: " + accountName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Balance: " + balance);
+    }
 }
